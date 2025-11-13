aa54126dc35b07dadb1e966991c6837de11e6a6b
BahromNigmanbekov <bahromnigmanbekov@gmail.com>
2025-11-13 23:20:05 +0500

    design o'zgartirildi

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,15 @@
 import React from "react";
+import { CgProfile } from "react-icons/cg";
+import { MdOutlineAccessTime } from "react-icons/md";
+
 
 function Navbar() {
   return (
     <nav className="navbar">
       <h1 className="logo">Timely</h1>
       <div className="nav-links">
-        <a href="#">Home</a>
-        <a href="#">Bookings</a>
-        <a href="#">Profile</a>
+        <a href="#"> <MdOutlineAccessTime /></a>
+        <a href="#"> <CgProfile /></a>
       </div>
     </nav>
   );
