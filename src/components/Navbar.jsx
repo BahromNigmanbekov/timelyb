0ea3fe71b64c785adc6f917f0bc911f9487852cc
BahromNigmanbekov <bahromnigmanbekov@gmail.com>
2025-11-16 11:02:43 +0500

    design o'zgartirildi

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { CgProfile } from "react-icons/cg";
 import { MdOutlineAccessTime } from "react-icons/md";
+import { IoMenu } from "react-icons/io5";
+import { FaPlusCircle } from "react-icons/fa";
 
 
 function Navbar() {
   return (
     <nav className="navbar">
-      <h1 className="logo">Timely</h1>
+      <h1 className="logo"><IoMenu /></h1>
       <div className="nav-links">
-        <a href="#"> <MdOutlineAccessTime /></a>
+        <a href="#"> <FaPlusCircle /></a>
         <a href="#"> <CgProfile /></a>
       </div>
     </nav>
